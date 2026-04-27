a9b9ef6b1f9b1b0d41b7779c07d0642b2e07a2ec
rainnasyahda8 <rainnasyahda8@gmail.com>
2026-04-27 17:58:09 +0700

    menambahkan include

--- fiendlassSatu.cpp
+++ fiendlassSatu.cpp
@@ -7,4 +7,40 @@ class orang
 {
 private:
     string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
+
+class siswa
+{
+private: 
+    int id;
+
+public:
+    void setId(int pId);
+    void displayA11(orang &a);
+};
+
+void siswa::displayA11(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+int main()
+{
+    orang joko;
+    joko.setNama("Joko Kumat");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayA11(joko);
+    return 0;
 }
